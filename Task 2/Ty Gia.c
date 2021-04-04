178ed5e3d9d6c76cf0cd180330e88d5aa059e7b8
linh1998vn <81789760+linh1998vn@users.noreply.github.com>
2021-04-04 15:00:24 +0700

    Task 2

---
+++ Task 2/Ty Gia.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) 
+{
+ 	int a,b;
+ 	
+ 	printf("Nhap so Do la:");
+ 	scanf("%d",&a);
+ 	
+ 	b=16689*a;
+ 	
+ 	printf("Tien viet la: %d",b);
+ 	
+	return 0;
+}
